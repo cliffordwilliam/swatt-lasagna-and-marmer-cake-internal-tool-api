aadb16e003c8a074ec8655197872b42bf56ba508
Clifford William <ccliffordwilliam@gmail.com>
2026-06-05 20:14:33 +0700

    feat: add pre commit and have it use ruff

--- app/domains/items/item_routers.py
+++ app/domains/items/item_routers.py
@@ -3,7 +3,7 @@ from .item_schema import Item
 
 router = APIRouter(prefix="/items")
 
+
 @router.post("")
 async def create_item(item: Item):
     return item
-
--- app/main.py
+++ app/main.py
@@ -3,8 +3,10 @@ from .domains.items import item_routers
 
 app = FastAPI()
 
+
 @app.get("/healthz")
 def health_check():
-    return { "status": "ok" }
+    return {"status": "ok"}
+
 
 app.include_router(item_routers.router)
