47d258b7a6d6058633262c031e2790aeff34a307
Clifford William <ccliffordwilliam@gmail.com>
2026-06-07 20:44:27 +0700

    docs: mention that deps is where we do the last commit

--- app/core/deps.py
+++ app/core/deps.py
@@ -1,7 +1,7 @@
 """Session provider.
 
 This module has API to give `request response unit` (UoW) owner a new Session state, owner can be in arbitrary location too.
-This hands off a new Session state to owner, and when owner is done we resume here to close the session.
+This hands off a new Session state to owner, and when owner is done we resume here to send commit to cluster and close the session.
 So it does not matter where owner is, we always close it here.
 """
 
